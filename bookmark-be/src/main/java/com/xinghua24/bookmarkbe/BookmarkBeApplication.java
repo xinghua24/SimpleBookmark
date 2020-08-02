e89150ace1546bb8847a1bb1655849bf2afa017c
Xinghua <xinghua24@gmail.com>
2020-08-01 21:48:53 -0400

    init commit

---
+++ bookmark-be/src/main/java/com/xinghua24/bookmarkbe/BookmarkBeApplication.java
@@ -0,0 +1,13 @@
+package com.xinghua24.bookmarkbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmarkBeApplication.class, args);
+	}
+
+}
---
+++ bookmark-be/src/test/java/com/xinghua24/bookmarkbe/BookmarkBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xinghua24.bookmarkbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
